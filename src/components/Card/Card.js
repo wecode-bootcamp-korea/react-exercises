1a80fa5a6ced8f4dc7e21b3b715574d92f0256f5
James Seung Hyun Soun <50141312+jamessoun93@users.noreply.github.com>
2020-05-19 16:21:07 +0900

    Revert "Feature/jintae"

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -18,16 +18,8 @@ import "./Card.css";
 ***********************************************************/
 
 class Card extends Component {
-  
   render() {
-    const {id, name, email } = this.props;
-    return (
-      <div className="card-container">
-        <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt="moster_img"/>
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
+    return <div className="card-container"></div>;
   }
 }
 
