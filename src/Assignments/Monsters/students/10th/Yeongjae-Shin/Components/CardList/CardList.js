122ca2b2ed9934e0caaf92da98a16f8a32b0f795
Yeongjae-Shin <syj9484@gmail.com>
2020-07-13 22:14:30 +0900

    Add: Monsters 과제 기능 구현 완료

--- src/Assignments/Monsters/students/10th/Yeongjae-Shin/Components/CardList/CardList.js
+++ src/Assignments/Monsters/students/10th/Yeongjae-Shin/Components/CardList/CardList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Card from "../Card/Card";
 import "./CardList.scss";
 
 /***********************************************************
@@ -9,7 +10,20 @@ import "./CardList.scss";
 
 class CardList extends Component {
   render() {
-    return <div className="card-list"></div>;
+    return (
+      <div className="card-list">
+        {this.props.monsters.map((monsters) => {
+          return (
+            <Card
+              key={monsters.id}
+              id={monsters.id}
+              name={monsters.name}
+              email={monsters.email}
+            />
+          );
+        })}
+      </div>
+    );
   }
 }
 
