989da145b785f8ccf5a5ac2f64c072fea55a9e3a
junggyoo <jungggyu.1@gmail.com>
2020-07-13 22:16:46 +0900

    [배정규] Add: api 데이터 로딩 처리 및 hadleChange 매소드 정의

--- src/Assignments/Monsters/students/10th/junggyoo/Monsters.js
+++ src/Assignments/Monsters/students/10th/junggyoo/Monsters.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import SearchBox from "./Components/SearchBox/SearchBox";
 import CardList from "./Components/CardList/CardList";
 import "./Monsters.scss";
@@ -23,20 +24,29 @@ import "./Monsters.scss";
 class Monsters extends Component {
   state = {
     monsters: [],
-    userInput: ""
+    userInput: "",
   };
 
   // 데이터 로딩
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((response) => this.setState({ monsters: response }));
+  }
+
   // SearchBox에 props로 넘겨줄 handleChange 메소드 정의
+
+  handleChange = (e) => {
+    this.setState({ userInput: e.target.value });
+  };
 
   render() {
     // 필터링 로직
-
     return (
       <div className="Monsters">
         <h1>컴포넌트 재사용 연습!</h1>
-        {/* <SearchBox handleChange=정의한메소드 /> */}
+        <SearchBox handleChange={this.handleChange} />
         {/* <CardList monsters=필터링 된 몬스터리스트 /> */}
       </div>
     );
