a0420bfde7e1d805f36c415ff80122a5616490b7
손승현 <jamessoun93@gmail.com>
2020-07-27 10:56:45 +0900

    Modify: main text2

--- src/Main.js
+++ src/Main.js
@@ -11,7 +11,7 @@ export default class Main extends Component {
         <div>MAIN PAGE</div>
         <div>MAIN PAGE</div>
         <div>MAIN PAGE</div>
-        <div>MAIN PAGE</div>
+        <div>TEST PAGE</div>
         <div>MAIN PAGE</div>
         <div>MAIN PAGE</div>
         <div>MAIN PAGE</div>
