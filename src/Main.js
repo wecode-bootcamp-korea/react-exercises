727c2a1817907b2adedb974b9bfa9a2c0477ee9b
James Seung Hyun Soun <50141312+jamessoun93@users.noreply.github.com>
2020-07-27 11:00:38 +0900

    Feature/testing (#17)
    * Modify: main text
    
    * Modify: main text2
    
    * Modify: main text3

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
-import Monsters from "./Assignments/Monsters/students/10th/7p3m1k/Monsters";
 
 export default class Main extends Component {
   render() {
     return (
       <>
-        <Monsters />
+        <div>MAIN PAGE</div>
+        <div>MAIN PAGE</div>
+        <div>MAIN PAGE</div>
+        <div>MAIN TEST</div>
+        <div>MAIN PAGE</div>
+        <div>MAIN PAGE</div>
+        <div>MAIN PAGE</div>
+        <div>TEST PAGE</div>
+        <div>MAIN PAGE</div>
+        <div>MAIN PAGE</div>
+        <div>MAIN TEST</div>
       </>
     );
   }
