80a521d10a0ab20b1a4b697510595e83934d518e
psy082 <zhappy114@gmail.com>
2020-08-11 22:14:38 +0900

    [김동호] ADD: Monster, CardList, Card 컴포넌트 작성

--- src/Assignments/Monsters/students/11th/psy082/Monsters.js
+++ src/Assignments/Monsters/students/11th/psy082/Monsters.js
@@ -23,19 +23,28 @@ import "./Monsters.scss";
 class Monsters extends Component {
   state = {
     monsters: [],
-    userInput: ""
+    userInput: "",
+    filtered: [],
   };
 
   // 데이터 로딩
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(res => this.setState({monsters: res}));
+  }
 
   // SearchBox 에 props로 넘겨줄 handleChange 메소드 정의
+  updateInput = ({target: {value}}) => {
+    this.setState({ userInput: value, filtered: this.state.monsters.filter(({name}) => name.toLowerCase().includes(value.toLowerCase()))});
+  }
 
   render() {
     return (
       <div className="Monsters">
         <h1>컴포넌트 재사용 연습!</h1>
-        {/* <SearchBox handleChange=정의한메소드 /> */}
-        {/* <CardList monsters=몬스터리스트 /> */}
+        <SearchBox handleChange={this.updateInput} />
+        <CardList monsters={this.state.filtered} />
       </div>
     );
   }
