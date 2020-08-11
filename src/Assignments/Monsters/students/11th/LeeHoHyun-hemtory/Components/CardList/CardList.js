6668b3ba27da0fb12768a2904472ef920f3b6490
lhh <jyslshsgdy@gmail.com>
2020-08-11 19:43:30 +0900

    Add: Monsters.js, CardList.js, Card.js 작업 - Monsters.js: fetch기능 추가 - Monsters.js: 이벤트 핸들러 추가 - Monsters.js: SearchBox filter 기능 추가 - CardList.js: Card map method 기능 추가 - Card.js: props 값 사용

--- src/Assignments/Monsters/students/11th/LeeHoHyun-hemtory/Components/CardList/CardList.js
+++ src/Assignments/Monsters/students/11th/LeeHoHyun-hemtory/Components/CardList/CardList.js
@@ -11,7 +11,11 @@ import "./CardList.scss";
 
 class CardList extends Component {
   render() {
-    return <div className="card-list"></div>;
+    return <div className="card-list">
+      {this.props.monsters.map((mon) => {
+        return <Card key={mon.id} id={mon.id} name={mon.name} email={mon.email}/>
+      })}    
+    </div>;
   }
 }
 
--- src/Assignments/Monsters/students/11th/LeeHoHyun-hemtory/Monsters.js
+++ src/Assignments/Monsters/students/11th/LeeHoHyun-hemtory/Monsters.js
@@ -27,15 +27,28 @@ class Monsters extends Component {
   };
 
   // 데이터 로딩
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(reponse => reponse.json())
+    .then(json => this.setState({monsters: json}));
+  }
 
   // SearchBox 에 props로 넘겨줄 handleChange 메소드 정의
+  handleChange = (e) => {
+    this.setState({userInput: e.target.value,
+      monsters: this.state.monsters.filter(mon => {
+        return mon.name.toLowerCase().includes(this.state.userInput);
+      })});
+  }
 
   render() {
     return (
       <div className="Monsters">
         <h1>컴포넌트 재사용 연습!</h1>
         {/* <SearchBox handleChange=정의한메소드 /> */}
+        <SearchBox handleChange={this.handleChange}/>
         {/* <CardList monsters=몬스터리스트 /> */}
+        <CardList monsters={this.state.monsters}/>
       </div>
     );
   }
