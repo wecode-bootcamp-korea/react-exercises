d5eeda231baab6d9ebc12ccf38bbb3017f2afc22
LeeHoHyun-hemtory <59634839+LeeHoHyun-hemtory@users.noreply.github.com>
2020-08-12 16:25:06 +0900

    Approve: 11기 이호현 님 과제
    * Add: Monsters.js, CardList.js, Card.js 작업
    - Monsters.js: fetch기능 추가
    - Monsters.js: 이벤트 핸들러 추가
    - Monsters.js: SearchBox filter 기능 추가
    - CardList.js: Card map method 기능 추가
    - Card.js: props 값 사용
    
    * Modify: SearchBox.js 수정
    - input 지우면 CardList 원복
    
    * Modify: Monsters.js, CardList.js 내용 수정
    - Monsters.js: arrow function return, {} 생략
    - Monsters.js: 변수명 수정
    - CardList.js: arrow function return, {} 생략

--- src/Assignments/Monsters/students/11th/LeeHoHyun-hemtory/Monsters.js
+++ src/Assignments/Monsters/students/11th/LeeHoHyun-hemtory/Monsters.js
@@ -27,15 +27,29 @@ class Monsters extends Component {
   };
 
   // 데이터 로딩
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(reponse => reponse.json())
+    .then(json => this.setState({monsters: json}));
+  }
 
   // SearchBox 에 props로 넘겨줄 handleChange 메소드 정의
+  handleChange = (e) => {
+    this.setState({userInput: e.target.value})
+  }
 
   render() {
+    let filterMonsters = this.state.monsters.filter(mon => 
+      (mon.name.toLowerCase().includes(this.state.userInput.toLowerCase()))
+    );
+    
     return (
       <div className="Monsters">
         <h1>컴포넌트 재사용 연습!</h1>
         {/* <SearchBox handleChange=정의한메소드 /> */}
+        <SearchBox handleChange={this.handleChange}/>
         {/* <CardList monsters=몬스터리스트 /> */}
+        <CardList monsters={filterMonsters}/>
       </div>
     );
   }
