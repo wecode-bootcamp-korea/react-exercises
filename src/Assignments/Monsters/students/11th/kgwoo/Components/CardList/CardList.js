6d558deac6ec623496e8797d942ffad9b792661e
김건우 <gunoo0126@gmail.com>
2020-08-12 16:44:41 +0900

    Add: Monsters 과제(기능구현) - jsonplaceholder의 API호출 - Card에 props로 id name email 전달 - map()사용하여, card를 cardlist에 배열만큼 전달 - filter() 사용하여, monsters 객체의 name과 input창의 value 값을 비교하여 일치하는 것을 띄워줌

--- src/Assignments/Monsters/students/11th/kgwoo/Components/CardList/CardList.js
+++ src/Assignments/Monsters/students/11th/kgwoo/Components/CardList/CardList.js
@@ -11,8 +11,14 @@ import "./CardList.scss";
 
 class CardList extends Component {
   render() {
-    return <div className="card-list"></div>;
+  return (
+    <div className="card-list">
+      {this.props.monsters.map((monsters) => {
+          return (
+            <Card key={monsters.id} id={monsters.id} name={monsters.name} email={monsters.email} />
+          );})}
+    </div>
+    );
   }
 }
-
 export default CardList;
