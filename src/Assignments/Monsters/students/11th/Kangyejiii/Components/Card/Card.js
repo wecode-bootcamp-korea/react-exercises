2db6af5e78e2dda2939dc4dfe1916355526e22ff
kangyeji <kyj07030911@gmail.com>
2020-08-13 22:26:19 +0900

    Refactor : 코드리뷰 반영 - 가독성을 위해 코드 스타일 수정( 띄어쓰기 ) - 줄일 수 있는 코드 수정 (`result.length===0` -> `!result.length`)

--- src/Assignments/Monsters/students/11th/Kangyejiii/Components/Card/Card.js
+++ src/Assignments/Monsters/students/11th/Kangyejiii/Components/Card/Card.js
@@ -18,18 +18,15 @@ import "./Card.scss";
 ***********************************************************/
 
 class Card extends Component {
-  render() {
-    return (
-      <div className="card-container">
-        <img
-          src={`https://robohash.org/${this.props.id}?set=set2&size=180x180`}
-          alt=""
-        />
-        <h2>{this.props.name}</h2>
-        <p>{this.props.email}</p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="card-container">
+				<img src={`https://robohash.org/${this.props.id}?set=set2&size=180x180`} alt="" />
+				<h2>{this.props.name}</h2>
+				<p>{this.props.email}</p>
+			</div>
+		);
+	}
 }
 
 export default Card;
--- src/Assignments/Monsters/students/11th/Kangyejiii/Components/CardList/CardList.js
+++ src/Assignments/Monsters/students/11th/Kangyejiii/Components/CardList/CardList.js
@@ -10,12 +10,14 @@ import "./CardList.scss";
 ***********************************************************/
 
 class CardList extends Component {
-  render() {
-    return <div className="card-list">
-      
-      {this.props.monsters.map((el)=>{return <Card key={el.id} id={el.id} name={el.name} email={el.email}></Card>})}
-    </div>;
-  }
+	render() {
+		return (
+			<div className="card-list">
+				{this.props.monsters.map( el => <Card key={el.id} id={el.id} name={el.name} email={el.email} />
+			)}
+			</div>
+		);
+	}
 }
 
 export default CardList;
