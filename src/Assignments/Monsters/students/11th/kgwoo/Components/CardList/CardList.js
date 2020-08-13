ffb28f93244608362c4ea4efa45d8851fe813f19
김건우 <gunoo0126@gmail.com>
2020-08-13 10:34:33 +0900

    Refactor: self-closing Tag 적용 - monsters.js 파일에 SearchBox 와 CardList에 sel-closing tag 적용

--- src/Assignments/Monsters/students/11th/kgwoo/Components/CardList/CardList.js
+++ src/Assignments/Monsters/students/11th/kgwoo/Components/CardList/CardList.js
@@ -11,13 +11,19 @@ import "./CardList.scss";
 
 class CardList extends Component {
   render() {
-  return (
-    <div className="card-list">
-      {this.props.monsters.map((monsters) => {
+    return (
+      <div className="card-list">
+        {this.props.monsters.map((monsters) => {
           return (
-            <Card key={monsters.id} id={monsters.id} name={monsters.name} email={monsters.email} />
-          );})}
-    </div>
+            <Card
+              key={monsters.id}
+              id={monsters.id}
+              name={monsters.name}
+              email={monsters.email}
+            />
+          );
+        })}
+      </div>
     );
   }
 }
