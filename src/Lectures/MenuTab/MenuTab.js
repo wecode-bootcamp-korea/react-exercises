5d7dc9b90ac0ffc08a2ac07c80e507a495d01dd2
오종택 <ohjtack@gmail.com>
2020-09-18 16:18:10 +0900

    최신화

--- src/Lectures/MenuTab/MenuTab.js
+++ src/Lectures/MenuTab/MenuTab.js
@@ -4,19 +4,43 @@ import Second from "./components/Second";
 import Third from "./components/Third";
 import "./MenuTab.scss";
 
-export default class MenuTab extends Component {
+class MenuTab extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentIdx: 1,
+    };
+  }
+
+  clickHandler = (idx) => {
+    console.log(idx);
+    this.setState({ currentIdx: idx });
+  };
+
   render() {
     return (
       <div className="wrapper">
         <ul className="tabs">
-          <li>First</li>
-          <li>Second</li>
-          <li>Third</li>
+          {CATEGORY_ARR.map((el, idx) => {
+            return (
+              <li key={el} onClick={() => this.clickHandler(idx + 1)}>
+                {el}
+              </li>
+            );
+          })}
         </ul>
-        <div className="contents">
-          <First />
-        </div>
+        <div className="contents">{MAPPING_OBJ[this.state.currentIdx]}</div>
       </div>
     );
   }
 }
+
+export default MenuTab;
+
+export const CATEGORY_ARR = ["First", "Second", "Third"];
+
+const MAPPING_OBJ = {
+  1: <First />,
+  2: <Second />,
+  3: <Third />,
+};
