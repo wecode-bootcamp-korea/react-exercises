b0cc24a4ea01ad53ca62af174ae1599da970f8bd
kangho <kangsick98@gmail.com>
2022-03-14 11:47:53 +0900

    Add: Monster Complate

--- src/Assignments/Monsters/Monsters.js
+++ src/Assignments/Monsters/Monsters.js
@@ -3,23 +3,6 @@ import SearchBox from "./Components/SearchBox/SearchBox";
 import CardList from "./Components/CardList/CardList";
 import "./Monsters.scss";
 
-/**********************************************************
-  API 주소: https://jsonplaceholder.typicode.com/users
-
-  1. 위 주소를 호출하여 데이터 로딩을 처리해주세요!
-    - useEffect()
-    - fetch
-    - setState (monsters 에 저장)
-
-  2. SearchBox 컴포넌트에 정의한 handleChange 메소드를 넘겨주고, 
-     호출 시 인자로 들어오는 이벤트객체(e)를 활용해 userInput 으로 setState.
-
-  3. 필터링 로직 구현 (filter 메소드 활용)
-      여기서 비교 대상은 monster 객체의 name 값입니다.
-      소문자로 바꾼 monster.name 값과 userInput값을 비교.
-      filter 메소드가 반환하는 값을 변수에 저장 후 return 문 안에 CardList에 props로 전달
-***********************************************************/
-
 function Monsters() {
   const [monsters, setMonsters] = useState([]);
   const [userInput, setUserInput] = useState("");
@@ -39,13 +22,13 @@ function Monsters() {
   };
 
   //userInput에 검색한 이름이 담김
-  console.log(userInput);
+  // console.log(userInput);
 
   const sortMonster = monsters.filter((monster) => {
     return monster.name.toLowerCase().includes(userInput);
   });
 
-  console.log(`sortMonster의 value는 ->`, sortMonster);
+  // console.log(`sortMonster의 value는 ->`, sortMonster);
 
   return (
     <div className="monsters">
