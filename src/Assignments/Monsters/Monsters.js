9035b2b27e77650027636ed552d833fa5b27c37d
minsu <cocacolazero024@gmail.com>
2022-03-27 22:00:38 +0900

    Task 2 Sending Props To CardList

--- src/Assignments/Monsters/Monsters.js
+++ src/Assignments/Monsters/Monsters.js
@@ -26,21 +26,18 @@ function Monsters() {
 
   // 데이터 로딩
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users', {
-      method: 'GET',
-    })
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((result) => setMonsters(result));
   }, []);
 
   // SearchBox 에 props로 넘겨줄 handleChange 메소드 정의
 
-  console.log(monsters);
   return (
     <div className='monsters'>
       <h1>컴포넌트 재사용 연습!</h1>
       {/* <SearchBox handleChange=정의한메소드 /> */}
-      {/* <CardList monsters=몬스터리스트 /> */}
+      <CardList monsters={monsters} />
     </div>
   );
 }
