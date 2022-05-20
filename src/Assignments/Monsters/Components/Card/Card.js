4522671639a8288ba4b0319607c7f5e09802dc15
Rey <Rey@Reys-MacBook-Pro.local>
2022-05-20 16:45:37 +0900

    [React] Monster

--- src/Assignments/Monsters/Components/Card/Card.js
+++ src/Assignments/Monsters/Components/Card/Card.js
@@ -17,8 +17,14 @@ import "./Card.scss";
   Name 과 Email 도 마찬가지입니다.
 ***********************************************************/
 
-function Card() {
-  return <div className="cardContainer"></div>;
+function Card({ id, name, email }) {
+  return (
+    <div className="cardContainer">
+    <img alt="monster" src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+    <h2>{name}</h2>
+    <p>{email}</p>
+  </div>
+  );
 }
 
 export default Card;
