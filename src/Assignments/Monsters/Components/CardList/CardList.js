02f34ffd4041d462328f09acda6e18addabff20d
Kyeom1997 <78855917+Kyeom1997@users.noreply.github.com>
2022-05-20 21:18:14 +0900

    [33기 김형겸] ADD: Monster 과제 완료

--- src/Assignments/Monsters/Components/CardList/CardList.js
+++ src/Assignments/Monsters/Components/CardList/CardList.js
@@ -9,8 +9,14 @@ import "./CardList.scss";
   Card 컴포넌트에서 필요로 하는 데이터는 id, name, email 입니다.
 ***********************************************************/
 
-function CardList() {
-  return <div className="cardList"></div>;
+function CardList(monsters) {
+  return (
+    <div className="cardList">
+      {monsters.map((monster) => {
+        return <Card monsters={monster} />;
+      })}
+    </div>
+  );
 }
 
 export default CardList;
