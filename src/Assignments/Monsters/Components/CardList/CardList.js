68f6886be11297f0e36b18df79a6c924500684db
haeun <yoohaaeun@gmail.com>
2022-05-20 21:56:51 +0900

    [33기 유하은] ADD: Monster 과제 완료

--- src/Assignments/Monsters/Components/CardList/CardList.js
+++ src/Assignments/Monsters/Components/CardList/CardList.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 // import Monsters from "../../Monsters";
-import Card from "../Card/Card";
-import "./CardList.scss";
+import Card from '../Card/Card';
+import './CardList.scss';
 
 /***********************************************************
   Card 컴포넌트를 import 한 뒤, props로 내려받은 데이터에 
@@ -9,8 +9,14 @@ import "./CardList.scss";
   Card 컴포넌트에서 필요로 하는 데이터는 id, name, email 입니다.
 ***********************************************************/
 
-function CardList() {
-  return <div className="cardList"></div>;
+function CardList({ monsters }) {
+  return (
+    <div className='cardList'>
+      {monsters.map((monster) => {
+        return <Card key={monster.id} monster={monster} />;
+      })}
+    </div>
+  );
 }
 
 export default CardList;
