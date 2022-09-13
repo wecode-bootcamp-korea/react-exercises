972c704f4d05d1c49c46d5a17ac9e43c065ea6d9
rayong <skdyds@gmail.com>
2022-09-13 13:40:57 +0900

    Add : API 로 데이터 호출, state 에 저장

--- src/Assignments/Monsters/Monsters.js
+++ src/Assignments/Monsters/Monsters.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBox from "./Components/SearchBox/SearchBox";
 import CardList from "./Components/CardList/CardList";
 import "./Monsters.scss";
@@ -25,6 +25,13 @@ function Monsters() {
   const [userInput, setUserInput] = useState("");
 
   // 데이터 로딩
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((result) => setMonsters(result));
+  }, []);
+
+  console.log(monsters);
 
   // SearchBox 에 props로 넘겨줄 handleChange 메소드 정의
 
