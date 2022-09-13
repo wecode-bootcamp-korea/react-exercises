73f2e1215693cf44515251c2b2d955e237222348
rayong <skdyds@gmail.com>
2022-09-13 14:00:55 +0900

    Add : props 데이터로 map 함수 호출해 card 컴포넌트 구현

--- src/Assignments/Monsters/Monsters.js
+++ src/Assignments/Monsters/Monsters.js
@@ -31,15 +31,13 @@ function Monsters() {
       .then((result) => setMonsters(result));
   }, []);
 
-  console.log(monsters);
-
   // SearchBox 에 props로 넘겨줄 handleChange 메소드 정의
 
   return (
     <div className="monsters">
       <h1>컴포넌트 재사용 연습!</h1>
       {/* <SearchBox handleChange=정의한메소드 /> */}
-      {/* <CardList monsters=몬스터리스트 /> */}
+      <CardList monsters={monsters} />
     </div>
   );
 }
