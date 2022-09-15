a82595e638d394de019dd647f4e969ee022d4d64
0taepark <ytpark1665@gmail.com>
2022-09-15 17:43:43 +0900

    first

--- src/Assignments/Monsters/Monsters.js
+++ src/Assignments/Monsters/Monsters.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import SearchBox from "./Components/SearchBox/SearchBox";
 import CardList from "./Components/CardList/CardList";
 import "./Monsters.scss";
@@ -25,14 +25,29 @@ function Monsters() {
   const [userInput, setUserInput] = useState("");
 
   // 데이터 로딩
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users" , {
+      method: "GET" ,
+  })
+    .then((res) => res.json())
+    .then((result) => setMonsters(result));
+  }, []);
 
   // SearchBox 에 props로 넘겨줄 handleChange 메소드 정의
+const updateUserInput = (e) => {
+  setUserInput(e.target.value);
+};  
+
+const sortedMonsters = monsters.filter((monster) => {
+  return monster.name.toLowerCase().includes(userInput.toLowerCase());
+})
+console.log(sortedMonsters);
 
   return (
     <div className="monsters">
       <h1>컴포넌트 재사용 연습!</h1>
-      {/* <SearchBox handleChange=정의한메소드 /> */}
-      {/* <CardList monsters=몬스터리스트 /> */}
+       <SearchBox handleChange={updateUserInput} />
+       <CardList monsters={sortedMonsters} />
     </div>
   );
 }
