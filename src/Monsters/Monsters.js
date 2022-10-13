0485787f51c73ae1d0d079d42ad64b0d9b186314
Hyun Choi <hyun@Hyunui-MacBookAir.local>
2022-10-13 22:21:19 +0900

    클론 후 브랜치생성

--- src/Monsters/Monsters.js
+++ src/Monsters/Monsters.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './Monsters.scss';
+import React, { useState } from "react"
+import "./Monsters.scss"
 
 function Monsters() {
+  const [monsterList, setMonsterList] = useState([])
+
   return (
-    <div className="monsters">
-      <h1 className="title">Monsters</h1>
-      <input className="search" placeholder="Search" />
+    <div className='monsters'>
+      <h1 className='title'>Monsters</h1>
+      <input className='search' placeholder='Search' />
     </div>
-  );
+  )
 }
 
-export default Monsters;
+export default Monsters
