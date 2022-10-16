baa61f918f3475541a71d43ca47b5c364132f0b3
w00jinkim <willjin1209@gmail.com>
2022-10-16 21:06:17 +0900

    [38기 김우진] ADD : 몬스터 따라 만들기 완성

--- src/Monsters/Monsters.js
+++ src/Monsters/Monsters.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import './Monsters.scss';
+import React, { useState, useEffect } from "react";
+import CardList from "./components/CardList/CardList";
+import "./Monsters.scss";
 
 function Monsters() {
+  const [monsterList, setMonsterList] = useState([]);
+  const [searchlog, setSearchlog] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((result) => result.json())
+      .then((data) => setMonsterList(data));
+  }, []);
+
+  const saveSearchlog = (event) => {
+    setSearchlog(event.target.value);
+  };
+
+  const filteredItem = monsterList.filter((monster) =>
+    monster.name.toLowerCase().includes(searchlog.toLowerCase())
+  );
+
   return (
     <div className="monsters">
       <h1 className="title">Monsters</h1>
-      <input className="search" placeholder="Search" />
+      <input
+        className="search"
+        placeholder="Search"
+        value={searchlog}
+        onChange={saveSearchlog}
+      />
+      <CardList monsterList={filteredItem} />
     </div>
   );
 }
