4278f16395736fd5ae3184d56f92626ca2e5d69f
Eunji Kang <etothej@naver.com>
2022-10-16 14:32:25 +0900

    ADD: 몬스터 검색기능 구현 완료

--- src/Monsters/Monsters.js
+++ src/Monsters/Monsters.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import './Monsters.scss';
+import React, { useEffect, useState } from "react";
+import "./Monsters.scss";
+import CardList from "../components/CardList/CardList";
 
 function Monsters() {
+  const [monsterList, setMonsterList] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((result) => result.json())
+      .then((data) => setMonsterList(data));
+  }, []);
+
+  const monsterSearch = (event) => {
+    setSearchKeyword(event.target.value);
+  };
+
+  const filteredItem = monsterList.filter((monster) => monster.name.toLowerCase().includes(searchKeyword.toLowerCase())
+  );
+
   return (
     <div className="monsters">
       <h1 className="title">Monsters</h1>
-      <input className="search" placeholder="Search" />
+      <input
+        className="search"
+        placeholder="Search"
+        value={searchKeyword}
+        onChange={monsterSearch}
+      />
+      <CardList monsterList={filteredItem} />
     </div>
   );
 }
