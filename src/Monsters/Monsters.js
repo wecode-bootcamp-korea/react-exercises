d32c6b50709256625326858df0f62dd1963b5530
Kyu heum Choi <kyuheumchoi@Kyuui-MacBookPro.local>
2022-10-16 13:52:58 +0900

    ADD : Assignment 제출

--- src/Monsters/Monsters.js
+++ src/Monsters/Monsters.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CardList from './components/CardList/CardList';
 import './Monsters.scss';
 
 function Monsters() {
+  const [monsterList, setMonsterList] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState('')
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((result) => result.json())
+      .then((data) => setMonsterList(data));
+  }, []);
+
+  const monsterSearch = (event) => {
+    setSearchKeyword(event.target.value);
+  };
+
+  const filteredItem = monsterList.filter((monster) => monster.name.toLowerCase().includes(searchKeyword.toLowerCase())
+  );
+
   return (
     <div className="monsters">
       <h1 className="title">Monsters</h1>
-      <input className="search" placeholder="Search" />
+      <input
+        className="search"
+        value={searchKeyword}
+        placeholder="Search"
+        onChange={monsterSearch}
+      />
+      <CardList monsterList={filteredItem} />
     </div>
   );
 }
