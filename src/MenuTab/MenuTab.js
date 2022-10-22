566e262a409c9cfdac7ff16ce6f18b909b687e01
w00jinkim <willjin1209@gmail.com>
2022-10-22 22:03:03 +0900

    [38기 김우진]ADD: Menu Tab 구현

--- src/MenuTab/MenuTab.js
+++ src/MenuTab/MenuTab.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import './MenuTab.scss';
+import React, { useState } from "react";
+import First from "./components/First";
+import Second from "./components/Second";
+import Third from "./components/Third";
+import "./MenuTab.scss";
 
 const MenuTab = () => {
+  const [currentTab, setCurrentTab] = useState("First");
+
   return (
     <div className="menuTab">
       <h1 className="title">Menu Tab</h1>
       <ul className="tabs">
-        <li>First</li>
-        <li>Second</li>
-        <li>Third</li>
+        {TAB_ARR.map((tab, index) => (
+          <li key={index} onClick={() => setCurrentTab(tab)}>
+            {tab}
+          </li>
+        ))}
       </ul>
-      <div className="contents">
-        {/* 메뉴에 따른 데이터가 들어가야 할 부분 */}
-      </div>
+      <div className="contents">{MAPPING_OBJ[currentTab]}</div>
     </div>
   );
 };
 
 export default MenuTab;
+
+const TAB_ARR = ["First", "Second", "Third"];
+
+const MAPPING_OBJ = {
+  First: <First />,
+  Second: <Second />,
+  Third: <Third />,
+};
