48a170442fcaf7ac37e0bff567784eb43dc26ac8
KMK99cone <105492581+KMK99cone@users.noreply.github.com>
2022-10-27 10:45:40 +0900

    [38기 김민경] menutab 과제 완료

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MenuTab from './MenuTab/MenuTab';
-import Monsters from './Monsters/Monsters';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MenuTab from "./MenuTab/MenuTab";
+import Monsters from "./Monsters/Monsters";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Monsters />} />
-        <Route path="/menuTab" element={<MenuTab />} />
+        <Route path="/menuTab" element={<Monsters />} />
+        <Route path="/" element={<MenuTab />} />
       </Routes>
     </BrowserRouter>
   );
