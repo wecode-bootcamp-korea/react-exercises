e3abd68640d93c2c4426ff7fd779104daef1bbe8
김기윤 <ki@Kis-MacBook-Pro.local>
2022-12-30 15:15:54 +0900

    Add: 몬스터 카드 및 검색 구현

--- src/Monsters/Monsters.js
+++ src/Monsters/Monsters.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CardList from './components/CardList/CardList';
 import './Monsters.scss';
 
 function Monsters() {
+  const [monsterList, setMonsterList] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const monsterSearch = (event) => {
+    setSearchKeyword(event.target.value);
+  };
+
+  const filteredItem = monsterList.filter((monster) =>
+    monster.name.toLowerCase().includes(searchKeyword.toLowerCase())
+  );
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((result) => result.json())
+      .then((data) => setMonsterList(data));
+  }, []);
+
   return (
-    <div className="monsters">
-      <h1 className="title">Monsters</h1>
-      <input className="search" placeholder="Search" />
+    <div className='monsters'>
+      <h1 className='title'>Monsters</h1>
+      <input
+        className='search'
+        value={searchKeyword}
+        placeholder='Search'
+        onChange={monsterSearch}
+      />
+      <CardList monsterList={filteredItem} />
     </div>
   );
 }
