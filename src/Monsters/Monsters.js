fcde6beca45e9ef496cd336a44c2c5653e130b74
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-07-15 16:05:25 +0900

    ADD : Monster Card 검색 구현완료

--- src/Monsters/Monsters.js
+++ src/Monsters/Monsters.js
@@ -2,7 +2,16 @@ import React, { useState, useEffect } from "react";
 import "./Monsters.scss";
 
 function Monsters() {
-  const [monstersData, setMonstersData] = useState();
+  const [monstersData, setMonstersData] = useState([]);
+  const [searchMonter, setSearchMonter] = useState("");
+
+  const onChange = (e) => {
+    setSearchMonter(e.target.value);
+  };
+
+  const filterMonster = monstersData.filter((monsterinfo) =>
+    monsterinfo.name.toLowerCase().includes(searchMonter.toLowerCase())
+  );
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -13,18 +22,24 @@ function Monsters() {
   return (
     <div className="monsters">
       <h1 className="title">Monsters</h1>
-      <input className="search" placeholder="Search" />
+      <input
+        className="search"
+        placeholder="Search"
+        onChange={onChange}
+        value={searchMonter}
+      />
       <ul>
-        {monstersData.map((monster) => (
-          <li className="monster" key={monster.id}>
-            <img
-              src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
-              alt={monster.name}
-            />
-            <p>{monster.name}</p>
-            <span>{monster.email}</span>
-          </li>
-        ))}
+        {monstersData &&
+          filterMonster.map((monster) => (
+            <li className="monster" key={monster.id}>
+              <img
+                src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+                alt={monster.name}
+              />
+              <p>{monster.name}</p>
+              <span>{monster.email}</span>
+            </li>
+          ))}
       </ul>
     </div>
   );
